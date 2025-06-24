e001f61925bedade50c284321c4bb21071b37428
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-06-24 13:49:12 +0300

    Add topBar

--- app/src/main/java/com/uesar/journal/MainActivity.kt
+++ app/src/main/java/com/uesar/journal/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,20 +29,33 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.uesar.journal.ui.theme.JournalTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         installSplashScreen()
+        enableEdgeToEdge()
         setContent {
             JournalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(), floatingActionButton = {
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                    TopAppBar(title = {
+                        Text("Your EchoJournal")
+                    }, actions = {
+                        IconButton(onClick = {}) {
+                            Icon(Icons.Outlined.Settings, contentDescription = null)
+                        }
+                    })
+                }, floatingActionButton = {
                     FloatingActionButton(onClick = {}) {
                         Icon(
                             Icons.Filled.Add, contentDescription = null
                         )
                     }
                 }) { innerPadding ->
-                    NoEntriesScreen(modifier = Modifier.padding(innerPadding))
+                    NoEntriesScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    )
                 }
             }
         }
@@ -46,10 +64,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NoEntriesScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Image(
-            painter = painterResource(id = R.drawable.no_entry),
-            contentDescription = null
+            painter = painterResource(id = R.drawable.no_entry), contentDescription = null
         )
         Text(modifier = Modifier.padding(top = 34.dp), text = "No Entries")
         Text("Start recording your first Echo")
