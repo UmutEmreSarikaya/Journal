301a8ae1f5ed85b7649728cf47495c0a95d54de5
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-07-08 17:48:07 +0300

    Add new entry screen

---
+++ app/src/main/java/com/uesar/journal/ui/newentry/NewEntryScreen.kt
@@ -0,0 +1,10 @@
+package com.uesar.journal.ui.newentry
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NewEntryScreen(modifier: Modifier = Modifier) {
+    Text("hi")
+}
