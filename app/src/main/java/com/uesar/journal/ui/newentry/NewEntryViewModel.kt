968706a4a6b818fb2268280b74b3cf3b63a20c05
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-07-27 18:35:56 +0300

    Implement actions for mood selector bottom sheet

--- app/src/main/java/com/uesar/journal/ui/newentry/NewEntryViewModel.kt
+++ app/src/main/java/com/uesar/journal/ui/newentry/NewEntryViewModel.kt
@@ -10,10 +10,33 @@ class NewEntryViewModel : ViewModel() {
     val state: StateFlow<NewEntryState> = _state
 
     fun onAction(action: NewEntryAction) {
-        when(action) {
+        when (action) {
             is NewEntryAction.OnTitleChanged -> {
                 _state.update { it.copy(title = action.title) }
             }
+
+            is NewEntryAction.BottomSheetClosed -> {
+                _state.update {
+                    it.copy(isBottomSheetOpen = false)
+                }
+            }
+
+            is NewEntryAction.OnChangeMoodClicked -> {
+                _state.update { it.copy(isBottomSheetOpen = true) }
+            }
+
+            is NewEntryAction.OnMoodIconClicked -> {
+                _state.update { it.copy(currentSelectedMoodInBottomSheet = action.selectedMood) }
+            }
+
+            is NewEntryAction.OnConfirmClicked -> {
+                _state.update { it.copy(selectedMood = action.selectedMood) }
+            }
+
+            is NewEntryAction.OnCancelMoodBottomSheet -> {
+                _state.update { it.copy(currentSelectedMoodInBottomSheet = state.value.selectedMood) }
+            }
+
             else -> Unit
         }
     }
