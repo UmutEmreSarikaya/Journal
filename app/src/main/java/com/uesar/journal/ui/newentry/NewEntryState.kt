b3ef64aa92b22dc2ed9c7e1208461c57a594e739
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-07-27 18:34:18 +0300

    Update new entry screen state

--- app/src/main/java/com/uesar/journal/ui/newentry/NewEntryState.kt
+++ app/src/main/java/com/uesar/journal/ui/newentry/NewEntryState.kt
@@ -1,5 +1,12 @@
 package com.uesar.journal.ui.newentry
 
+import com.uesar.journal.Mood
+
 data class NewEntryState(
-    val title: String = ""
+    val title: String = "",
+    val description: String = "",
+    val topics: List<String> = emptyList(),
+    val isBottomSheetOpen: Boolean = false,
+    val currentSelectedMoodInBottomSheet: Mood? = null,
+    val selectedMood: Mood? = null
 )
