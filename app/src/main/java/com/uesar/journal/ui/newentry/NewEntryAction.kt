6aa62a8bc852c34f000c4c583c2645cae57abe1c
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-07-27 18:35:23 +0300

    Add actions for mood selector bottom sheet

--- app/src/main/java/com/uesar/journal/ui/newentry/NewEntryAction.kt
+++ app/src/main/java/com/uesar/journal/ui/newentry/NewEntryAction.kt
@@ -1,6 +1,13 @@
 package com.uesar.journal.ui.newentry
 
+import com.uesar.journal.Mood
+
 sealed interface NewEntryAction {
     data class OnTitleChanged(val title: String) : NewEntryAction
+    data class OnMoodIconClicked(val selectedMood: Mood) : NewEntryAction
+    data class OnConfirmClicked(val selectedMood: Mood) : NewEntryAction
     data object NavigateBack : NewEntryAction
+    data object BottomSheetClosed : NewEntryAction
+    data object OnChangeMoodClicked : NewEntryAction
+    data object OnCancelMoodBottomSheet : NewEntryAction
 }
