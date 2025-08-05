40a9cf655366207d0083ac1d8031447ea8999358
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-08-05 12:33:12 +0300

    Add type converters

---
+++ app/src/main/java/com/uesar/journal/data/local/mapper/AppTypeConverter.kt
@@ -0,0 +1,50 @@
+package com.uesar.journal.data.local.mapper
+
+import androidx.room.TypeConverter
+import com.uesar.journal.Mood
+import com.uesar.journal.Recording
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.util.Date
+
+class AppTypeConverter {
+    @TypeConverter
+    fun fromStringList(list: List<String>): String {
+        return Json.encodeToString(list)
+    }
+
+    @TypeConverter
+    fun toStringList(data: String): List<String> {
+        return Json.decodeFromString(data)
+    }
+
+    @TypeConverter
+    fun fromDate(date: Date): Long {
+        return date.time
+    }
+
+    @TypeConverter
+    fun toDate(timestamp: Long): Date {
+        return Date(timestamp)
+    }
+
+    @TypeConverter
+    fun fromMood(mood: Mood): String {
+        return Json.encodeToString(mood)
+    }
+
+    @TypeConverter
+    fun toMood(data: String): Mood {
+        return Json.decodeFromString(data)
+    }
+
+    @TypeConverter
+    fun fromRecording(recording: Recording): String {
+        return Json.encodeToString(recording)
+    }
+
+    @TypeConverter
+    fun toRecording(data: String): Recording {
+        return Json.decodeFromString(data)
+    }
+}
