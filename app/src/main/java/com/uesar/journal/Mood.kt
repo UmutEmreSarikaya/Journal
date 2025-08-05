581f5818e3a18331382da458699c72b0c627b112
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-08-05 12:37:54 +0300

    Add Serializable annotation to Mood

--- app/src/main/java/com/uesar/journal/Mood.kt
+++ app/src/main/java/com/uesar/journal/Mood.kt
@@ -1,7 +1,9 @@
 package com.uesar.journal
 
 import androidx.annotation.DrawableRes
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Mood(
     @DrawableRes val icon: Int,
     @DrawableRes val emptyIcon: Int,
