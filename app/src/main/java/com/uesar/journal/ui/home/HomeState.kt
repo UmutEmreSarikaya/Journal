aad8920f8c1b0dc2eac1814a00a9e97ad4aedf62
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-08-13 17:10:28 +0300

    Add list of recordings to state

--- app/src/main/java/com/uesar/journal/ui/home/HomeState.kt
+++ app/src/main/java/com/uesar/journal/ui/home/HomeState.kt
@@ -1,9 +1,11 @@
 package com.uesar.journal.ui.home
 
 import com.uesar.journal.domain.JournalEntry
+import com.uesar.journal.domain.Recording
 
 data class HomeState(
     val journalEntries: List<JournalEntry> = emptyList(),
+    val recordings: List<Recording> = emptyList(),
     val isBottomSheetOpen: Boolean = false,
     val isRecording: Boolean = false,
     val recordingTime: String = "00:00:00"
