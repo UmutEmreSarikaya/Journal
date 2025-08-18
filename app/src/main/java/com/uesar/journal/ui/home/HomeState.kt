91ecb98b0ecf8dd13e32e072ac4a8e650bb6e36d
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-08-18 17:05:50 +0300

    Update states to use JournalEntryUIState

--- app/src/main/java/com/uesar/journal/ui/home/HomeState.kt
+++ app/src/main/java/com/uesar/journal/ui/home/HomeState.kt
@@ -1,12 +1,10 @@
 package com.uesar.journal.ui.home
 
-import com.uesar.journal.domain.JournalEntry
-import com.uesar.journal.domain.Playback
+import com.uesar.journal.ui.model.JournalEntryUIState
 
 data class HomeState(
-    val journalEntries: List<JournalEntry> = emptyList(),
-    val playbacks: List<Playback> = emptyList(),
+    val journalEntries: List<JournalEntryUIState> = emptyList(),
     val isBottomSheetOpen: Boolean = false,
     val isRecording: Boolean = false,
     val recordingTime: String = "00:00:00"
-) 
+)
--- app/src/main/java/com/uesar/journal/ui/newentry/NewEntryState.kt
+++ app/src/main/java/com/uesar/journal/ui/newentry/NewEntryState.kt
@@ -1,17 +1,13 @@
 package com.uesar.journal.ui.newentry
 
 import com.uesar.journal.Mood
-import com.uesar.journal.domain.Playback
+import com.uesar.journal.ui.model.JournalEntryUIState
 
 data class NewEntryState(
-    val title: String = "",
-    val playback: Playback = Playback(false, "00:00", "00:00", ""),
+    val journalEntryUIState: JournalEntryUIState = JournalEntryUIState(),
     val topicText: String = "",
-    val description: String = "",
-    val topics: List<String> = emptyList(),
     val isTopicDropDownOpen: Boolean = false,
     val isBottomSheetOpen: Boolean = false,
     val currentSelectedMoodInBottomSheet: Mood? = null,
-    val selectedMood: Mood? = null,
     val isNavigationDialogOpen: Boolean = false
 )
