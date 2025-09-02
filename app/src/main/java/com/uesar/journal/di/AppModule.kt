088d616e4d5fe17a263c54ff23b4d0ef6d7e8321
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-09-02 15:39:47 +0300

    Add datastore

--- app/src/main/java/com/uesar/journal/di/AppModule.kt
+++ app/src/main/java/com/uesar/journal/di/AppModule.kt
@@ -1,5 +1,9 @@
 package com.uesar.journal.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.room.Room
 import com.uesar.journal.AudioPlayerImpl
 import com.uesar.journal.AudioPlayer
@@ -8,7 +12,9 @@ import com.uesar.journal.AudioRecorderImpl
 import com.uesar.journal.data.local.AppDatabase
 import com.uesar.journal.data.local.JournalEntryDao
 import com.uesar.journal.data.local.JournalRepositoryImpl
+import com.uesar.journal.data.settings.DataStoreSettings
 import com.uesar.journal.domain.JournalRepository
+import com.uesar.journal.domain.settings.SettingsPreferences
 import com.uesar.journal.ui.home.HomeViewModel
 import com.uesar.journal.ui.newentry.NewEntryViewModel
 import com.uesar.journal.ui.settings.SettingsViewModel
@@ -18,13 +24,16 @@ import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
 val appModule = module {
     viewModelOf(::HomeViewModel)
     viewModelOf(::NewEntryViewModel)
     viewModelOf(::SettingsViewModel)
-    singleOf(::AudioRecorderImpl).bind<AudioRecorder>()
-    singleOf(::AudioPlayerImpl).bind<AudioPlayer>()
-    singleOf(::JournalRepositoryImpl).bind<JournalRepository>()
+    singleOf(::AudioRecorderImpl) bind AudioRecorder::class
+    singleOf(::AudioPlayerImpl) bind AudioPlayer::class
+    singleOf(::JournalRepositoryImpl) bind JournalRepository::class
+    singleOf(::DataStoreSettings) bind SettingsPreferences::class
     single {
         Room.databaseBuilder(
             androidApplication(),
