aa0c30bf620c9441eb68819cf9c14e8903837bc6
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-09-17 18:02:30 +0300

    Remove FormatTime.kt

--- app/src/main/java/com/uesar/journal/ui/utils/FormatTime.kt
+++
@@ -1,7 +0,0 @@
-package com.uesar.journal.ui.utils
-
-fun formatSecondsToMinutes(seconds: Int): String {
-    val minutes = seconds / 60
-    val remainingSeconds = seconds % 60
-    return "%02d:%02d".format(minutes, remainingSeconds)
-}
