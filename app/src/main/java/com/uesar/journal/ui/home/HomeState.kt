8a983a8b3bfdcc0883ddc63067c4b07e1f821ee1
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-09-18 13:14:42 +0300

    Add filter states

--- app/src/main/java/com/uesar/journal/ui/home/HomeState.kt
+++ app/src/main/java/com/uesar/journal/ui/home/HomeState.kt
@@ -6,5 +6,7 @@ data class HomeState(
     val journalEntries: List<JournalEntryUIState> = emptyList(),
     val isBottomSheetOpen: Boolean = false,
     val isRecording: Boolean = false,
-    val recordingTime: String = "00:00:00"
+    val recordingTime: String = "00:00:00",
+    val isFilterPopupOpen: Boolean = false,
+    val filterList: List<String> = emptyList()
 )
